8797664b2b5b6175cd9c4d15840201cb6aa57955
liamellison02 <liam.ellison.codes@gmail.com>
2024-08-09 13:42:00 -0400

    gunicorn setup

--- config.py
+++ config.py
@@ -2,16 +2,16 @@ import os
 from flask import Flask, jsonify
 from flask_cors import CORS
 from dotenv import load_dotenv
-
-app = Flask(__name__)
-CORS(app)
 load_dotenv()
 
+from flaskapp import create_app
 
 class Config:
     OPENAI_API_KEY = os.environ.get('SECRET_KEY')
     tidb_connection_string = os.environ.get('TIDB_DATABASE_URI')
 
+app = create_app(Config())
+CORS(app)
 
 @app.route('/', methods=['GET'])
 def index():
